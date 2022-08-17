828581b1d2da7e45be9839b366108969b0ad5b2f
saintmaxi <36748278+saintmaxi@users.noreply.github.com>
2022-08-17 05:51:03 -0500

    dark

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,14 +1,67 @@
 // a saintmaxi joint
 // onChainDiscordDirectory created by @0xInuarashi
 
+if (checkDarkModeOn()) {
+    applyDarkMode(true);
+}
+
+function toggleDarkMode() {
+    let darkModeOn = localStorage.getItem("darkMode");
+    if (darkModeOn == "true") {
+        localStorage.setItem("darkMode", "false");
+        applyDarkMode(false);
+    }
+    else {
+        localStorage.setItem("darkMode", "true");
+        applyDarkMode(true);
+    }
+}
+
+function checkDarkModeOn() {
+    let darkModeOn = localStorage.getItem("darkMode");
+    return darkModeOn == "true";
+}
+
+function applyDarkMode(turnOn) {
+    if (turnOn) {
+        $("#dark-mode-toggle").attr("src", "/images/sun-solid.svg");
+        $("#dark-mode-toggle-mobile").attr("src", "/images/sun-solid.svg");
+        $(".twitter img").attr("src", "/images/twitter-black.png");
+        $(".opensea img").attr("src", "/images/os-white.png");
+        $(".discord img").attr("src", "/images/dc-white.png");
+        $(".etherscan img").attr("src", "/images/etherscan-white.png");
+        $(".down-arrow").attr("src", "/images/down-arrow-white.png");
+        $(".body").addClass("dark");
+        $("#roadmap").addClass("dark");
+        $("#navbar").addClass("dark");
+        $(".header-menu a").addClass("dark");
+        $(".title-link").addClass("dark");
+    }
+    else {
+        $("#dark-mode-toggle").attr("src", "/images/moon-solid.svg");
+        $("#dark-mode-toggle-mobile").attr("src", "/images/moon-solid.svg");
+        $(".twitter img").attr("src", "/images/twitter-white.png");
+        $(".opensea img").attr("src", "/images/os-black.png");
+        $(".discord img").attr("src", "/images/dc-black.png");
+        $(".etherscan img").attr("src", "/images/etherscan-black.png");
+        $(".down-arrow").attr("src", "/images/down-arrow-black.png");
+        $(".body").removeClass("dark");
+        $("#roadmap").removeClass("dark");
+        $("#navbar").removeClass("dark");
+        $(".header-menu a").removeClass("dark");
+        $(".title-link").removeClass("dark");
+    }
+}
+
 async function displayErrorMessage(message, timed = true) {
     if (!($("#error-popup").length)) {
-        let fakeJSX = `<div id="error-popup">
+        let darkMode = checkDarkModeOn() ? "dark" : "";
+        let fakeJSX = `<div id="error-popup" class="${darkMode}">
                             <div class="popup-wrapper">
                                 <svg id="close" onclick="$('#block-screen-error').remove();$('#error-popup').remove();" xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 512 512">
                                 <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-                                <path fill="black"
+                                <path fill="${darkMode ? "white" : "black"}"
                                 d="M0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256zM175 208.1L222.1 255.1L175 303C165.7 312.4 165.7 327.6 175 336.1C184.4 346.3 199.6 346.3 208.1 336.1L255.1 289.9L303 336.1C312.4 346.3 327.6 346.3 336.1 336.1C346.3 327.6 346.3 312.4 336.1 303L289.9 255.1L336.1 208.1C346.3 199.6 346.3 184.4 336.1 175C327.6 165.7 312.4 165.7 303 175L255.1 222.1L208.1 175C199.6 165.7 184.4 165.7 175 175C165.7 184.4 165.7 199.6 175 208.1V208.1z" />
                                 </svg>
                                 <p>${message}</p>
@@ -27,12 +80,13 @@ async function displayErrorMessage(message, timed = true) {
 
 async function displayStatusMessage(message) {
     if (!($("#status-popup").length)) {
-        let fakeJSX = `<div id="status-popup">
+        let darkMode = checkDarkModeOn() ? "dark" : "";
+        let fakeJSX = `<div id="status-popup" class="${darkMode}">
                             <div class="popup-wrapper">
                                 <svg id="close" onclick="$('#block-screen-status').remove();$('#status-popup').remove();" xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 512 512">
                                 <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-                                <path fill="black"
+                                <path fill="${darkMode ? "white" : "black"}"
                                 d="M0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256zM175 208.1L222.1 255.1L175 303C165.7 312.4 165.7 327.6 175 336.1C184.4 346.3 199.6 346.3 208.1 336.1L255.1 289.9L303 336.1C312.4 346.3 327.6 346.3 336.1 336.1C346.3 327.6 346.3 312.4 336.1 303L289.9 255.1L336.1 208.1C346.3 199.6 346.3 184.4 336.1 175C327.6 165.7 312.4 165.7 303 175L255.1 222.1L208.1 175C199.6 165.7 184.4 165.7 175 175C165.7 184.4 165.7 199.6 175 208.1V208.1z" />
                                 </svg>
                                 <p>${message}</p>
